248a370a7f4d1c168ba4513eecc5197f5fab0be4
Samrat Malisetti <samratmalisetti@gmail.com>
2024-03-06 09:51:34 -0800

    active style link for navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,13 @@
+'use client';
+
+import classNames from 'classnames';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
 
 const Navbar = () => {
+    const currentPath = usePathname();
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' },
@@ -14,7 +19,11 @@ const Navbar = () => {
                 {links.map(link =>
                     <Link
                         key={link.href}
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                        className={classNames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors': true,
+                        })}
                         href={link.href}>
                         {link.label}
                     </Link>)}
