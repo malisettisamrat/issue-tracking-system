a12b4188fd933e1d30bdeb917f892527f40bd083
Samrat Malisetti <samratmalisetti@gmail.com>
2024-03-07 01:42:02 -0800

    refactor imports

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Spinner = () => {
+const Spinner = () => {
   return (
     <div
       className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
@@ -11,3 +11,5 @@ export const Spinner = () => {
     </div>
   )
 }
+
+export default Spinner;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,11 @@
 import { Table } from '@radix-ui/themes'
 import React from 'react'
 import prisma from '@/prisma/client'
-import IssueStatusBadge from '../components/IssueStatusBadge'
-import delay from 'delay'
+import { IssueStatusBadge, Link } from '@/app/components'
 import IssueActions from './IssueActions'
-import Link from '../components/Link'
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
   return (
     <div>
       <IssueActions />
