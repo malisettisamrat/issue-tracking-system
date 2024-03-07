b917596c19b99af7b65977bae3cb46ad1f5f31db
Samrat Malisetti <samratmalisetti@gmail.com>
2024-03-07 12:06:59 -0800

    added delete issue button

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -1,8 +1,8 @@
-import { IssueStatusBadge } from '@/app/components'
-import { Issue } from '@prisma/client'
-import { Heading, Flex, Card, Text } from '@radix-ui/themes'
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Heading, Flex, Card, Text } from "@radix-ui/themes";
+import React from "react";
+import ReactMarkdown from "react-markdown";
 
 const IssueDetails = ({ issue }: { issue: Issue }) => {
   return (
@@ -12,11 +12,11 @@ const IssueDetails = ({ issue }: { issue: Issue }) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card className='prose' mt="4">
+      <Card className="prose max-w-full" mt="4">
         <ReactMarkdown>{issue?.description}</ReactMarkdown>
       </Card>
     </>
-  )
-}
+  );
+};
 
-export default IssueDetails
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import prisma from '@/prisma/client'
-import { notFound } from 'next/navigation'
-import { Box, Grid } from '@radix-ui/themes'
-import EditIssueButton from './EditIssueButton'
-import IssueDetails from './IssueDetails'
+import React from "react";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import { Box, Flex, Grid } from "@radix-ui/themes";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./delete/DeleteIssueButton";
 
 const IssueDetailsPage = async ({ params }: { params: { id: string } }) => {
   const issue = await prisma.issue.findUnique({
     where: {
-      id: parseInt(params.id)
-    }
-  })
+      id: parseInt(params.id),
+    },
+  });
 
-  if (!issue)
-    notFound();
+  if (!issue) notFound();
 
   return (
-    <Grid columns={{ initial: "1", md: "2" }} gap="3">
-      <Box>
+    <Grid columns={{ initial: "1", sm: "5" }} gap="3">
+      <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <EditIssueButton issue={issue} />
+        <Flex direction="column" gap="3">
+          <EditIssueButton issueId={issue.id} />
+          <DeleteIssueButton issueId={issue.id} />
+        </Flex>
       </Box>
     </Grid>
-  )
-}
+  );
+};
 
-export default IssueDetailsPage
+export default IssueDetailsPage;
