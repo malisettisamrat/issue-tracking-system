da5bb411f6f18fc403daa78c8b57d1226b34dc20
Samrat Malisetti <samratmalisetti@gmail.com>
2024-03-09 20:52:28 -0800

    build the issues summary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,39 @@
+import { Status } from "@prisma/client";
+import { Card, Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueSummary = ({ open, inProgress, closed }: Props) => {
+  const containers: { label: string; value: number; status: Status }[] = [
+    { label: "Open Issues", value: open, status: "OPEN" },
+    { label: "Closed Issues", value: closed, status: "CLOSED" },
+    { label: "In Progress Issues", value: inProgress, status: "IN_PROGRESS" },
+  ];
+  return (
+    <Flex gap="3">
+      {containers.map((container) => (
+        <Card key={container.label}>
+          <Flex direction="column" gap="1">
+            <Link
+              className="text-sm font-medium"
+              href={`/issues/list?status=${container.status}`}
+            >
+              {container.label}
+            </Link>
+            <Text size="5" className="font-bold">
+              {container.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+};
+
+export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
+import prisma from "@/prisma/client";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const openIssues = await prisma.issue.count({ where: { status: "OPEN" } });
+  const closedIssues = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  const inProgressIssues = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  return (
+    <IssueSummary
+      open={openIssues}
+      inProgress={closedIssues}
+      closed={inProgressIssues}
+    />
+  );
 }
